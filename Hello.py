80c3db3193894e208e32ff41f106b7fbe240216f
am1785 <allenmo88@gmail.com>
2022-12-17 22:48:45 -0500

    basic HelloWorld standalone app using flask

--- Hello.py
+++ Hello.py
@@ -1,9 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+'''
+Tutorial on venv
+
+# Create a new virtualenv named "myproject"
+
+$ virtualenv myproject
+
+# Activate the virtualenv (OS X & Linux)
+$ source myproject/Scripts/activate
+
+# To deactive virtualenv
+$ deactivate
+
+'''
+
 @app.route('/')
 def hello_world():
    return 'Hello World'
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
