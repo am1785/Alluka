337c6e7904d8cc86c3ede7e97f7d16442ad59a29
am1785 <allenmo88@gmail.com>
2022-12-18 03:28:06 -0500

    updated rest API SearchResult Get function using flask_rest

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from . import db
 sys.path.append("Soara/")
 
 from flask import Flask, jsonify
+from flask_restful import Api, Resource
 from config import Config
 # from flask_sqlalchemy import SQLAlchemy
 
@@ -14,6 +15,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_object(Config)
     app.secret_key = os.getenv("SECRET_KEY")
+    api = Api(app)
     db.init_app(app)
 
     if test_config is None:
@@ -28,7 +30,7 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    # flask --app flaskr init-db
+    # flask --app Soara init-db
     # flask --app Soara --debug run
 
     # a simple page that says hi
@@ -36,21 +38,46 @@ def create_app(test_config=None):
     def hi():
         return 'Hi!'
 
-    @app.route('/search/q=<string:q>', methods=['GET'])
-    # protect against injections: https://github.com/TryGhost/node-sqlite3/issues/57
-    def search_corpus(q):
-        cursor = db.get_db()
-        count = cursor.execute(
-        'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;',
-        ('% '+q+' %',)).fetchone()
+# restful implementation of Flask API
 
-        result = cursor.execute(
-        'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;', 
-        ('% '+q+' %',)).fetchall()
-        result = [tuple(row) for row in result]
-        result.append(tuple(count))
-        db.close_db()
-        return jsonify(result)
+    class SearchResult(Resource):
+        def get(self, q, random):
+            cursor = db.get_db()
+            select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
+            count = cursor.execute(
+            select_query,
+            ('% '+q+' %',)).fetchone()
+
+            if random==1:
+                select_query = 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;'
+            else:
+                select_query = 'SELECT text FROM corpus WHERE text LIKE ? LIMIT 10;'
+            result = cursor.execute(
+            select_query,
+            ('% '+q+' %',)).fetchall()
+            result = [tuple(row) for row in result]
+            result.append(tuple(count))
+            db.close_db()
+            return jsonify(result)
+
+    api.add_resource(SearchResult, "/search/q=<string:q>&r=<int:random>")
+
+    # NATIVE implementation of REST API
+    # @app.route('/search/q=<string:q>', methods=['GET'])
+    # # protect against injections: https://github.com/TryGhost/node-sqlite3/issues/57
+    # def search_corpus(q):
+        # cursor = db.get_db()
+        # count = cursor.execute(
+        # 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;',
+        # ('% '+q+' %',)).fetchone()
+
+        # result = cursor.execute(
+        # 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;', 
+        # ('% '+q+' %',)).fetchall()
+        # result = [tuple(row) for row in result]
+        # result.append(tuple(count))
+        # db.close_db()
+        # return jsonify(result)
 
     # adding regexp implementation: https://github.com/thomasnield/oreilly_intermediate_sql_for_data/issues/5
 
