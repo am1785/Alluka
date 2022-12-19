3bad205c841029a9e600fee0fd14e00640918ff6
am1785 <allenmo88@gmail.com>
2022-12-19 04:08:56 -0500

    got native rest api to work with require login decorator

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ sys.path.append("Soara/")
 from flask import Flask, jsonify, redirect, render_template
 from flask_restful import Api, Resource
 from config import Config
+from Soara.auth import login_required
 # from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None):
@@ -39,51 +40,58 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hi():
         return 'Hi!'
-
-    @app.route('/index')
+    @app.route('/')
+    @login_required
     def index():
         return render_template('index.html')
 
 # restful implementation of Flask API
 
-    class SearchResult(Resource):
-        def get(self, q, random):
-            cursor = db.get_db()
-            select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
-            count = cursor.execute(
-            select_query,
-            ('% '+q+' %',)).fetchone()
+    # class SearchResult(Resource):
+    #     def get(self, q, random):
+    #         cursor = db.get_db()
+    #         select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
+    #         count = cursor.execute(
+    #         select_query,
+    #         ('% '+q+' %',)).fetchone()
 
-            if random==1:
-                select_query = 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;'
-            else:
-                select_query = 'SELECT text FROM corpus WHERE text LIKE ? LIMIT 10;'
-            result = cursor.execute(
-            select_query,
-            ('% '+q+' %',)).fetchall()
-            result = [tuple(row) for row in result]
-            result.append(tuple(count))
-            db.close_db()
-            return jsonify(result)
+    #         if random==1:
+    #             select_query = 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;'
+    #         else:
+    #             select_query = 'SELECT text FROM corpus WHERE text LIKE ? LIMIT 10;'
+    #         result = cursor.execute(
+    #         select_query,
+    #         ('% '+q+' %',)).fetchall()
+    #         result = [tuple(row) for row in result]
+    #         result.append(tuple(count))
+    #         db.close_db()
+    #         return jsonify(result)
 
-    api.add_resource(SearchResult, "/search/q=<string:q>&r=<int:random>")
+    # api.add_resource(SearchResult, "/search/q=<string:q>&r=<int:random>")
 
     # NATIVE implementation of REST API
-    # @app.route('/search/q=<string:q>', methods=['GET'])
-    # # protect against injections: https://github.com/TryGhost/node-sqlite3/issues/57
-    # def search_corpus(q):
-        # cursor = db.get_db()
-        # count = cursor.execute(
-        # 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;',
-        # ('% '+q+' %',)).fetchone()
+    @login_required
+    @app.route('/search/q=<string:q>&r=<int:random>', methods=['GET'])
+    # protect against injections: https://github.com/TryGhost/node-sqlite3/issues/57
+    def search_corpus(q, random):
+        cursor = db.get_db()
+        select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
+        count = cursor.execute(
+        select_query,
+        ('% '+q+' %',)).fetchone()
 
-        # result = cursor.execute(
-        # 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;', 
-        # ('% '+q+' %',)).fetchall()
-        # result = [tuple(row) for row in result]
-        # result.append(tuple(count))
-        # db.close_db()
-        # return jsonify(result)
+        if random==1:
+            select_query = 'SELECT text FROM corpus WHERE text LIKE ? ORDER BY RANDOM() LIMIT 10;'
+        else:
+            select_query = 'SELECT text FROM corpus WHERE text LIKE ? LIMIT 10;'
+
+        result = cursor.execute(
+        select_query,
+        ('% '+q+' %',)).fetchall()
+        result = [tuple(row) for row in result]
+        result.append(tuple(count))
+        db.close_db()
+        return jsonify(result)
 
     # adding regexp implementation: https://github.com/thomasnield/oreilly_intermediate_sql_for_data/issues/5
 
