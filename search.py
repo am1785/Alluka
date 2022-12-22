c29dc04087f2df0fbd3e681b884e8e7d559dd1ce
am1785 <allenmo88@gmail.com>
2022-12-22 15:49:00 -0500

    implemented POS search function and UI updates

--- search.py
+++ search.py
@@ -11,30 +11,62 @@ from Soara.db import get_db
 bp = Blueprint('search', __name__)
 # soara_endpoint = "http://127.0.0.1:5000/"
 
+import sqlite3
+import re
+
+def match(expr, item):
+    reg = re.compile(expr, re.I)
+    return reg.search(item) is not None
+
 @login_required
 @bp.route('/search/<string:q>/<int:page>', methods=['GET', 'POST'])
 def search_corpus(q, page):
     # print(request.args.get('string:q'))
     if request.method == 'GET':
+        pos = ".*" # the pos regex query
         q = q.strip()
-        print(f"searching for {q}")
+        q_unchanged = q
+        if match(r'(\<[A-Z]{1,4}\>)|(\<\.\>)', q):
+            # Method: 2 pass regex filtering (Query + POS)
+            # q = Hello \w+ my name is \w+
+            # pos = \w+ PRON \w+ \w+ NOUN
+            all_pos = ['NOUN','PRON','ADJ','VERB','ADV','ADP','PRT','DET','CONJ','NUM']
+            print("GRAMMAR SEARCH:")
+            print(f"plain q: {q}")
+            # pos_exp = re.findall(r'\<(.*?)\>', q)
+            q_tmp = re.findall(r'[a-zA-Z0-9]+', q)
+            pos = []
+            print(q)
+            for i,v in enumerate(q_tmp):
+                if v in all_pos:
+                    q_tmp[i] = "[a-zA-Z0-9]+"
+                    pos.append(v)
+                else:
+                    pos.append("[A-Z.]+")
+            q = " ".join(q_tmp)
+            pos = " ".join(pos)
+            # print(f'q = {q}')
+            # print(f'pos = {pos}')
+
         cursor = get_db()
-        select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
+        cursor.create_function("REGEXP", 2, match)
+        select_query = 'SELECT COUNT(*) FROM corpus WHERE text REGEXP ? AND pos REGEXP ?;'
+
         count = cursor.execute(
         select_query,
-        ('% '+q+' %',)).fetchone()
+        (q,pos)).fetchone()
         total_pages = int(math.ceil(int(tuple(count)[0]) / 30 + 0.01))
         pages = list(range(1, total_pages+1))
 
-        print(f"total pages: {total_pages}")
-        count = str(tuple(count)[0]) + " Total Results for \'" + q + "\'."
+        count = str(tuple(count)[0]) + " Total Results for \'" + q_unchanged + "\'."
         offset = (page-1) * 30
 
-        select_query = 'SELECT channel, text, videos.vid, CAST(timestamp as INTEGER) FROM corpus INNER JOIN videos ON corpus.vid = videos.vid WHERE text LIKE ? LIMIT 30 OFFSET ?;'
+        select_query = 'SELECT channel, text, videos.vid, CAST(timestamp as INTEGER) FROM corpus INNER JOIN videos ON corpus.vid = videos.vid WHERE text REGEXP ? AND pos REGEXP ? LIMIT 30 OFFSET ?;'
 
         result = cursor.execute(
         select_query,
-        ('% '+q+' %', offset,)).fetchall()
+        # ('% '+q+' %', offset,)).fetchall()
+        (q, pos, offset,)).fetchall()
         result = [tuple(row) for row in result]
         result.append([count])
         cursor.close()
@@ -46,27 +78,30 @@ def search_corpus(q, page):
         page = 1
         if request.form.get('page_no'):
             page = request.form['page_no']
-        # return redirect(url_for("search.search_corpus", q=query, page=page))
-        cursor = get_db()
-        select_query = 'SELECT COUNT(*) FROM corpus WHERE text LIKE ?;'
-        count = cursor.execute(
-        select_query,
-        ('% '+q+' %',)).fetchone()
-        total_pages = int(math.ceil(int(tuple(count)[0]) / 30 + 0.01))
-        pages = list(range(1, total_pages+1))
+        return redirect(url_for("search.search_corpus", q=q, page=page), code=301)
+        # cursor = get_db()
+        # cursor.create_function("REGEXP", 2, match)
+        # select_query = 'SELECT COUNT(*) FROM corpus WHERE text REGEXP ?;'
+        # count = cursor.execute(
+        # select_query,
+        # # ('% '+q+' %',)).fetchone()
+        # (q,)).fetchone()
+        # total_pages = int(math.ceil(int(tuple(count)[0]) / 30 + 0.01))
+        # pages = list(range(1, total_pages+1))
 
-        count = str(tuple(count)[0]) + " Total Results for \'" + q + "\'."
-        offset = (page-1) * 30
+        # count = str(tuple(count)[0]) + " Total Results for \'" + q + "\'."
+        # offset = (page-1) * 30
 
-        select_query = 'SELECT channel, text, videos.vid, CAST(timestamp as INTEGER) FROM corpus INNER JOIN videos ON corpus.vid = videos.vid WHERE text LIKE ? LIMIT 30 OFFSET ?;'
+        # select_query = 'SELECT channel, text, videos.vid, CAST(timestamp as INTEGER) FROM corpus INNER JOIN videos ON corpus.vid = videos.vid WHERE text REGEXP ? LIMIT 30 OFFSET ?;'
 
-        result = cursor.execute(
-        select_query,
-        ('% '+q+' %', offset,)).fetchall()
-        result = [tuple(row) for row in result]
-        result.append([count])
-        cursor.close()
-        return render_template('index.html', results = result, pages = pages, params = q)
+        # result = cursor.execute(
+        # select_query,
+        # # ('% '+q+' %', offset,)).fetchall()
+        # (q, offset,)).fetchall()
+        # result = [tuple(row) for row in result]
+        # result.append([count])
+        # cursor.close()
+        # return render_template('index.html', results = result, pages = pages, params = q)
 
 @bp.route('/', methods=['GET', 'POST'])
 @login_required
--- static/search.js
+++ static/search.js
@@ -5,12 +5,12 @@ function highlightResult(query) {
 var texts = document.getElementsByClassName('resultText');  // the div to change
 // directly using innerHTML creates a vector for CSS attacks by injecting HTML code, using DOMPurify to sanitize input.
 // https://github.com/cure53/DOMPurify
+const pattern = /\<(.*?)\>/ig;
+query = query.replaceAll(pattern, '\\w+');
 
 Array.from(texts).forEach(element => {
-    element.innerHTML = (DOMPurify.sanitize(element.innerHTML.replace(new RegExp('('+query+')','ig'), '<span class=highlight>$1</span>')));
+    element.innerHTML = (DOMPurify.sanitize(element.innerHTML.replace(new RegExp('('+ query +')','ig'), '<span class="badge badge-info" >$1</span>')));
 });
-
-// console.log(query);
 }
 
 const query = String(document.getElementsByClassName("resultCount")[0].textContent);
@@ -18,23 +18,56 @@ var highlightWord = 'NONE';
 query !== "No results" ? highlightWord = query.match(new RegExp("\'(.*?)\'", "ig"))[0]: highlightWord = 'NONE';
 highlightWord = highlightWord.substring(1, highlightWord.length-1);
 
-console.log(highlightWord);
-// window.onload = highlightResult(highlightWord);
-// const queryInput = document.getElementById('query');
-// queryInput.addEventListener('keyup',function(e){console.log(e)});
-// const form = document.getElementById('searchForm');
-// form.addEventListener('submit', function(e){console.log(e.target.childNodes[0]);})
-
-// console.log(form.childNodes);
+// console.log(highlightWord);
 
 if (document.readyState == 'complete') {
-    // highlightWord = queryInput.value;
     highlightResult(highlightWord);
 } else {
     document.onreadystatechange = function () {
         if (document.readyState === "complete") {
-            // highlightWord = queryInput.value;
             highlightResult(highlightWord);
         }
     }
+}
+const queryInput = document.getElementById('query');
+const noun = document.getElementById('nounBtn');
+const pron = document.getElementById('pronBtn');
+const adj = document.getElementById('adjBtn');
+const verb = document.getElementById('verbBtn');
+const adv = document.getElementById('advBtn');
+const adp = document.getElementById('adpBtn');
+const prt = document.getElementById('prtBtn');
+const det = document.getElementById('detBtn');
+const conj = document.getElementById('conjBtn');
+const num = document.getElementById('numBtn');
+
+function addNoun(){
+    queryInput.value += "<" + noun.textContent + "> ";
+}
+function addPron(){
+    queryInput.value += "<" + pron.textContent + "> ";
+}
+function addAdj(){
+    queryInput.value += "<" + adj.textContent + "> ";
+}
+function addVerb(){
+    queryInput.value += "<" + verb.textContent + "> ";
+}
+function addAdv(){
+    queryInput.value += "<" + adv.textContent + "> ";
+}
+function addAdp(){
+    queryInput.value += "<" + adp.textContent + "> ";
+}
+function addPrt(){
+    queryInput.value += "<" + prt.textContent + "> ";
+}
+function addDet(){
+    queryInput.value += "<" + det.textContent + "> ";
+}
+function addConj(){
+    queryInput.value += "<" + conj.textContent + "> ";
+}
+function addNum(){
+    queryInput.value += "<" + num.textContent + "> ";
 }
